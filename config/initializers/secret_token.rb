b28e8557fdd8babff3b7cc8af387e0db36b23e71
bushizhang <bushizhang@gmail.com>
2013-05-18 12:15:06 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upinhair::Application.config.secret_token = '5253305c2d36afa3da2a9efd0a0909326d72a97b83268b6283088c4966065df2430dbe5f62b298852085fa92e0304abfe7a0082bb59439cc22343d1648a59220'

b75af6b2ca0827d1d80e5985d7e94a09b4b131b0
bushizhang <bushizhang@gmail.com>
2013-05-18 18:22:17 -0400

    dashboard layout

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,8 @@ Upinhair::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
 
+  match '/dashboard', :to => 'home#dashboard'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
